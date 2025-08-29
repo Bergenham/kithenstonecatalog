b11c307ff2f5804805c810157b9c0ce6ef2ddf9f
Bergenham <yura.pechenkin.v@gmail.com>
2025-08-29 18:37:44 +0300

    исправления все ajax-филтров+диномическое подтягивание филтров

--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,18 +1,21 @@
 from django.core.exceptions import ValidationError
 from django.views.generic import ListView
 from .models import QuartzStone, StoneImage, CeramicsStone, AcrylicStone, NaturalStone
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpResponse
 from django.http import JsonResponse
 from basic_page.models import UserBidModel
 from django.http import HttpResponseNotFound
+from django.core.paginator import Paginator
+from django.template.loader import render_to_string
+from django.db.models import Count
 
 def custom_404_view(request, exception):
     return HttpResponseNotFound(render(request, 'fronttemp/404.html'))
 
 def stone_detail_q(request, stone_id=None):
     if stone_id:
-        stone = get_object_or_404(QuartzStone, pk=stone_id)
-        images = StoneImage.objects.filter(stone=stone, archive=False).all()
+        stone = get_object_or_404(QuartzStone.objects.filter(archive=False), pk=stone_id)
+        images = StoneImage.objects.filter(stone=stone).all()
     else:
         return redirect(reverse("basic_page:main"))
 
@@ -46,7 +49,7 @@ def stone_detail_q(request, stone_id=None):
 
 def stone_detail_c(request, stone_id=None):
     if stone_id:
-        stone = get_object_or_404(CeramicsStone, pk=stone_id)
+        stone = get_object_or_404(CeramicsStone.objects.filter(archive=False), pk=stone_id)
         images = StoneImage.objects.filter(stone=stone, archive=False).all()
     else:
         return redirect(reverse("basic_page:main"))
@@ -81,7 +84,7 @@ def stone_detail_c(request, stone_id=None):
 
 def stone_detail_a(request, stone_id=None):
     if stone_id:
-        stone = get_object_or_404(AcrylicStone, pk=stone_id)
+        stone = get_object_or_404(AcrylicStone.objects.filter(archive=False), pk=stone_id)
         images = StoneImage.objects.filter(stone=stone, archive=False).all()
     else:
         return redirect(reverse("basic_page:main"))
@@ -116,7 +119,7 @@ def stone_detail_a(request, stone_id=None):
 
 def stone_detail_n(request, stone_id=None):
     if stone_id:
-        stone = get_object_or_404(NaturalStone, pk=stone_id)
+        stone = get_object_or_404(NaturalStone.objects.filter(archive=False), pk=stone_id)
         images = StoneImage.objects.filter(stone=stone, archive=False).all()
     else:
         return redirect(reverse("basic_page:main"))
@@ -153,6 +156,7 @@ class QuartzCatalog(ListView):
     template_name = 'fronttemp/quartz_catalog.html'
     context_object_name = 'stones'
     ordering = ['name_stone']
+    paginate_by = None \
 
     def get_queryset(self):
         queryset = QuartzStone.objects.filter(archive=False).only(
@@ -188,17 +192,38 @@ class QuartzCatalog(ListView):
         context['selected_textures'] = self.request.GET.getlist('texture')
         context['selected_fakturas'] = self.request.GET.getlist('faktura')
 
-        context['brand_choices'] = QuartzStone.BrandStoneChoices.choices
-        context['color_choices'] = QuartzStone.ColorChoices.choices
-        context['texture_choices'] = QuartzStone.TextureChoices.choices
-        context['faktura_choices'] = QuartzStone.FakturaChoices.choices
+        stones_qs = QuartzStone.objects.filter(archive=False)
+        context['brand_options'] = list(
+            stones_qs.order_by('brand_stone').values_list('brand_stone', flat=True).distinct()
+        )
+        context['color_options'] = list(
+            stones_qs.order_by('color').values_list('color', flat=True).distinct()
+        )
+        context['texture_options'] = list(
+            stones_qs.order_by('texture').values_list('texture', flat=True).distinct()
+        )
+        context['faktura_options'] = list(
+            stones_qs.order_by('faktura').values_list('faktura', flat=True).distinct()
+        )
 
         return context
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            html = render_to_string('fronttemp/_stones_grid.html', context, request=request)
+            return HttpResponse(html)
+
+        return self.render_to_response(context)
+
 
 class CeramicCatalog(ListView):
     template_name = 'fronttemp/ceramic_catalog.html'
     context_object_name = 'stones'
     ordering = ['name_stone']
+    paginate_by = None \
 
     def get_queryset(self):
         queryset = CeramicsStone.objects.filter(archive=False).only(
@@ -234,17 +259,37 @@ class CeramicCatalog(ListView):
         context['selected_textures'] = self.request.GET.getlist('texture')
         context['selected_fakturas'] = self.request.GET.getlist('faktura')
 
-        context['brand_choices'] = CeramicsStone.BrandStoneChoices.choices
-        context['color_choices'] = CeramicsStone.ColorChoices.choices
-        context['texture_choices'] = CeramicsStone.TextureChoices.choices
-        context['faktura_choices'] = CeramicsStone.FakturaChoices.choices
+        stones_qs = CeramicsStone.objects.filter(archive=False)
+        context['brand_options'] = list(
+            stones_qs.order_by('brand_stone').values_list('brand_stone', flat=True).distinct()
+        )
+        context['color_options'] = list(
+            stones_qs.order_by('color').values_list('color', flat=True).distinct()
+        )
+        context['texture_options'] = list(
+            stones_qs.order_by('texture').values_list('texture', flat=True).distinct()
+        )
+        context['faktura_options'] = list(
+            stones_qs.order_by('faktura').values_list('faktura', flat=True).distinct()
+        )
 
         return context
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            html = render_to_string('fronttemp/_stones_grid.html', context, request=request)
+            return HttpResponse(html)
+
+        return self.render_to_response(context)
 
 class NaturalCatalog(ListView):
     template_name = 'fronttemp/natural_catalog.html'
     context_object_name = 'stones'
     ordering = ['name_stone']
+    paginate_by = None \
 
     def get_queryset(self):
         queryset = NaturalStone.objects.filter(archive=False).only(
@@ -280,17 +325,37 @@ class NaturalCatalog(ListView):
         context['selected_textures'] = self.request.GET.getlist('texture')
         context['selected_fakturas'] = self.request.GET.getlist('faktura')
 
-        context['brand_choices'] = NaturalStone.BrandStoneChoices.choices
-        context['color_choices'] = NaturalStone.ColorChoices.choices
-        context['texture_choices'] = NaturalStone.TextureChoices.choices
-        context['faktura_choices'] = NaturalStone.FakturaChoices.choices
+        stones_qs = NaturalStone.objects.filter(archive=False)
+        context['brand_options'] = list(
+            stones_qs.order_by('brand_stone').values_list('brand_stone', flat=True).distinct()
+        )
+        context['color_options'] = list(
+            stones_qs.order_by('color').values_list('color', flat=True).distinct()
+        )
+        context['texture_options'] = list(
+            stones_qs.order_by('texture').values_list('texture', flat=True).distinct()
+        )
+        context['faktura_options'] = list(
+            stones_qs.order_by('faktura').values_list('faktura', flat=True).distinct()
+        )
 
         return context
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            html = render_to_string('fronttemp/_stones_grid.html', context, request=request)
+            return HttpResponse(html)
+
+        return self.render_to_response(context)
 
 class AcrilCatalog(ListView):
     template_name = 'fronttemp/acril_catalog.html'
     context_object_name = 'stones'
     ordering = ['name_stone']
+    paginate_by = None \
 
     def get_queryset(self):
         queryset = AcrylicStone.objects.filter(archive=False).only(
@@ -326,9 +391,28 @@ class AcrilCatalog(ListView):
         context['selected_textures'] = self.request.GET.getlist('texture')
         context['selected_fakturas'] = self.request.GET.getlist('faktura')
 
-        context['brand_choices'] = AcrylicStone.BrandStoneChoices.choices
-        context['color_choices'] = AcrylicStone.ColorChoices.choices
-        context['texture_choices'] = AcrylicStone.TextureChoices.choices
-        context['faktura_choices'] = AcrylicStone.FakturaChoices.choices
+        stones_qs = AcrylicStone.objects.filter(archive=False)
+        context['brand_options'] = list(
+            stones_qs.order_by('brand_stone').values_list('brand_stone', flat=True).distinct()
+        )
+        context['color_options'] = list(
+            stones_qs.order_by('color').values_list('color', flat=True).distinct()
+        )
+        context['texture_options'] = list(
+            stones_qs.order_by('texture').values_list('texture', flat=True).distinct()
+        )
+        context['faktura_options'] = list(
+            stones_qs.order_by('faktura').values_list('faktura', flat=True).distinct()
+        )
 
         return context
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            html = render_to_string('fronttemp/_stones_grid.html', context, request=request)
+            return HttpResponse(html)
+
+        return self.render_to_response(context)
